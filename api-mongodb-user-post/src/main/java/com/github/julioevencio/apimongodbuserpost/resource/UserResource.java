064b02e7b6049074a41bc94eba35db5a1ba4f66c
JulioEvencio <julioigreja.dev@gmail.com>
2023-11-06 22:04:33 -0300

    added: doc for user resource

--- api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/resource/UserResource.java
+++ api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/resource/UserResource.java
@@ -3,6 +3,7 @@ package com.github.julioevencio.apimongodbuserpost.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.julioevencio.apimongodbuserpost.domain.User;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/users")
+@Tag(name = "Users", description = "Endpoints for users")
 public class UserResource {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(
+			summary = "Return all users",
+			description = "Return all users",
+			tags = {"Users"},
+			responses = {
+					@ApiResponse(
+							responseCode = "200",
+							description = "Return all users",
+							content = @Content(
+									mediaType = MediaType.APPLICATION_JSON_VALUE,
+									array = @ArraySchema(schema = @Schema(implementation = User.class))
+							)
+					)
+			}
+	)
 	public ResponseEntity<List<User>> findAll() {
 		User fulano = new User("1", "Fulano", "fulano@fulano");
 		User ciclano = new User("2", "Ciclano", "ciclano@ciclano");

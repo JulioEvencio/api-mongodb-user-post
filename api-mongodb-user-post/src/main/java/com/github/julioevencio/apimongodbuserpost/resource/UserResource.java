a7be379a49d5e7358eab0ad37d605ca1b14bcad3
JulioEvencio <julioigreja.dev@gmail.com>
2023-11-08 20:34:31 -0300

    added: insert user

--- api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/resource/UserResource.java
+++ api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.github.julioevencio.apimongodbuserpost.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.julioevencio.apimongodbuserpost.domain.User;
 import com.github.julioevencio.apimongodbuserpost.dto.UserDTO;
@@ -83,6 +86,28 @@ public class UserResource {
 		UserDTO response = new UserDTO(user);
 
 		return ResponseEntity.ok().body(response);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Operation(
+			summary = "Insert an user",
+			description = "Insert an user",
+			tags = {"Users"},
+			responses = {
+					@ApiResponse(
+							responseCode = "201",
+							description = "Insert an user"
+					)
+			}
+	)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO dto) {
+		User user = service.fromDTO(dto);
+		
+		user = service.insert(user);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/services/UserService.java
+++ api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.julioevencio.apimongodbuserpost.domain.User;
+import com.github.julioevencio.apimongodbuserpost.dto.UserDTO;
 import com.github.julioevencio.apimongodbuserpost.repositories.UserRepository;
 import com.github.julioevencio.apimongodbuserpost.services.exceptions.ObjectNotFoundException;
 
@@ -24,6 +25,14 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
 
 }

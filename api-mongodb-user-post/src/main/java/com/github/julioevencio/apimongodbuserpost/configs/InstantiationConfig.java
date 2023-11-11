b0f541ed2c8b1a76d4fc3b0ca45649e57a46e233
JulioEvencio <julioigreja.dev@gmail.com>
2023-11-10 23:16:19 -0300

    added: author dto

--- api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/configs/InstantiationConfig.java
+++ api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/configs/InstantiationConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.github.julioevencio.apimongodbuserpost.domain.Post;
 import com.github.julioevencio.apimongodbuserpost.domain.User;
+import com.github.julioevencio.apimongodbuserpost.dto.AuthorDTO;
 import com.github.julioevencio.apimongodbuserpost.repositories.PostRepository;
 import com.github.julioevencio.apimongodbuserpost.repositories.UserRepository;
 
@@ -33,11 +34,12 @@ public class InstantiationConfig implements CommandLineRunner {
 		User fulano = new User(null, "fulano", "fulano@fulano");
 		User ciclano = new User(null, "ciclano", "ciclano@ciclano");
 		User beltrano = new User(null, "beltrano", "beltrano@beltrano");
-
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", fulano);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", fulano);
-
+		
 		userRepository.saveAll(Arrays.asList(fulano, ciclano, beltrano));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(fulano));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(fulano));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 

6edf0def5760a9101cd91290f1351d7ab28bb6d9
JulioEvencio <julioigreja.dev@gmail.com>
2023-11-12 00:42:52 -0300

    added: comments for post

--- api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/configs/InstantiationConfig.java
+++ api-mongodb-user-post/src/main/java/com/github/julioevencio/apimongodbuserpost/configs/InstantiationConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.github.julioevencio.apimongodbuserpost.domain.Post;
 import com.github.julioevencio.apimongodbuserpost.domain.User;
 import com.github.julioevencio.apimongodbuserpost.dto.AuthorDTO;
+import com.github.julioevencio.apimongodbuserpost.dto.CommentDTO;
 import com.github.julioevencio.apimongodbuserpost.repositories.PostRepository;
 import com.github.julioevencio.apimongodbuserpost.repositories.UserRepository;
 
@@ -40,6 +41,13 @@ public class InstantiationConfig implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(fulano));
 		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(fulano));
 
+		CommentDTO comment1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(ciclano));
+		CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("21/03/2018"), new AuthorDTO(beltrano));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("21/03/2018"), new AuthorDTO(ciclano));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		fulano.getPosts().addAll(Arrays.asList(post1, post2));
